657ef0696a9de73e654563f04c84dd9e1449cf1b
manyak-yuriy <manyak.yuriy@gmail.com>
2016-10-28 01:20:19 +0300

    Added buttons and handlers

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+var posData = {};
+
 $(  function() 
     {
+           
+
             $(".draggable" ).draggable();
 
             $(".resizeable" ).resizable();
@@ -9,13 +13,53 @@ $(  function()
             $('#posSaver').click(
                 function() 
                 {
+                  
                   $(".draggable").each( 
                     function() 
                     { 
-                      console.log($(this).text()); 
+                      //console.log($(this).text()); 
+                      var key = $(this).attr("id");
+                      posData[key] = {
+                                         left: $(this).css("left"), 
+                                         top: $(this).css("top"), 
+                                         width: $(this).css("width"), 
+                                         height: $(this).css("height")
+                                     };
                     }
                   );
+
+                  console.log();
                 }
+
+                
             );
+
+            $('#posLoader').click(
+                function() 
+                {
+                  
+                  $(".draggable").each( 
+                    function() 
+                    { 
+                      //console.log($(this).text()); 
+                      var key = $(this).attr("id");
+                      
+
+                      $(this).css("left", posData[key]["left"]);
+                      $(this).css("top", posData[key]["top"]);
+                      $(this).css("width", posData[key]["width"]);
+                      $(this).css("height", posData[key]["height"]);
+
+                    }
+                  );
+
+                  console.log();
+                }
+
+                
+            );
+
+            
+
    } 
 );
